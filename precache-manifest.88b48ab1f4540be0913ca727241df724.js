4cec8418da689d1aa61c408bfc329c8e4e11d7ff
doberman257 <doberman257@gmail.com>
2020-05-05 23:07:38 +0200

    Updates

---
+++ precache-manifest.88b48ab1f4540be0913ca727241df724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9508c93946386aa2c618a90c031613e3",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "cbf89c84a124e69ba930",
+    "url": "/corona/static/css/main.6b6435d7.chunk.css"
+  },
+  {
+    "revision": "95f1d727d6ca509cf919",
+    "url": "/corona/static/js/2.c6f80511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona/static/js/2.c6f80511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf89c84a124e69ba930",
+    "url": "/corona/static/js/main.5959c915.chunk.js"
+  },
+  {
+    "revision": "b711712299ab2066e5bf",
+    "url": "/corona/static/js/runtime-main.c1a05405.js"
+  }
+]);
